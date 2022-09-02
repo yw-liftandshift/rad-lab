e8ef9d1db5cf95697a7367ce17bdbacdc0a9b719
Maksym Veliaminov <maksym.veliaminov@yoppworks.com>
2022-09-02 15:33:27 -0400

    Add budget to cromwell

--- modules/genomics_cromwell/main.tf
+++ modules/genomics_cromwell/main.tf
@@ -148,3 +148,71 @@ resource "google_storage_bucket_object" "service" {
   source = "scripts/build/cromwell.service"
   bucket = google_storage_bucket.cromwell_workflow_bucket.name
 }
+
+resource "google_billing_budget" "budget" {
+  billing_account = var.billing_account_id
+  display_name    = "Billing Budget for ${var.project_name} project"
+
+  budget_filter {
+    projects = ["projects/${var.project_name}"]
+    custom_period { 
+      start_date {
+        year = var.budget_start_date_year
+        month = var.budget_start_date_month
+        day = var.budget_start_date_day
+      }
+      end_date {
+        year = var.budget_end_date_year
+        month = var.budget_end_date_month
+        day = var.budget_end_date_day
+      }
+    }
+  }
+
+  amount {
+    specified_amount {
+      currency_code = var.budget_currency_code
+      units         = var.budget_amount
+    }
+  }
+  threshold_rules {
+    threshold_percent = 1.0
+  }
+  threshold_rules {
+    threshold_percent = 0.9
+  }
+  threshold_rules {
+    threshold_percent = 0.5
+  }
+  threshold_rules {
+    threshold_percent = 0.25
+  }
+
+  all_updates_rule {
+    monitoring_notification_channels = [
+      google_monitoring_notification_channel.scientist_notification_channel.id,
+      google_monitoring_notification_channel.manager_notification_channel.id
+    ]
+    disable_default_iam_recipients = true
+  }
+}
+
+resource "google_monitoring_notification_channel" "scientist_notification_channel" {
+  display_name = "Budget Notification Channel for scientist"
+  type         = "email"
+  project      = module.project_radlab_genomics.project_id
+
+  labels = {
+    email_address = var.owner
+  }
+}
+
+resource "google_monitoring_notification_channel" "manager_notification_channel" {
+  display_name = "Budget Notification Channel for manager"
+  type         = "email"
+  project      = module.project_radlab_genomics.project_id
+
+  labels = {
+    email_address = var.manager
+  }
+}
