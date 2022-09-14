0422abf83985d565482593c33cdb10cbb67c1704
Maksym Veliaminov <maksym.veliaminov@yoppworks.com>
2022-09-14 09:29:46 -0400

    Add wait time

--- modules/genomics_dsub/main.tf
+++ modules/genomics_dsub/main.tf
@@ -201,15 +201,19 @@ resource "google_storage_bucket_iam_member" "sa_p_ngs_output_bucket" {
   ]
 }
 
-# resource "time_sleep" "wait_iam_binding" {
-#   depends_on = [
-#     google_project_iam_binding.genomics_ngs_user_role1,
-#     google_storage_bucket_iam_member.sa_p_ngs_input_bucket,
-#     google_storage_bucket_iam_member.sa_p_ngs_output_bucket
-#   ]
+resource "time_sleep" "wait_iam_binding" {
+  depends_on = [
+    null_resource.build_and_push_image,
+    google_project_iam_member.gcs_sa_pubsub_publisher,
+    google_storage_bucket_iam_member.sa_p_ngs_input_bucket,
+    google_storage_bucket_iam_member.sa_p_ngs_output_bucket,
+    google_storage_bucket_iam_binding.binding1,
+    google_storage_bucket.input_bucket,
+    google_storage_bucket.output_bucket
+  ]
 
-#   create_duration = "180s"
-# }
+  create_duration = "180s"
+}
 
 resource "google_cloudfunctions2_function" "function" {
   provider    = google-beta
@@ -264,7 +268,8 @@ resource "google_cloudfunctions2_function" "function" {
     google_storage_bucket_iam_member.sa_p_ngs_output_bucket,
     google_storage_bucket_iam_binding.binding1,
     google_storage_bucket.input_bucket,
-    google_storage_bucket.output_bucket
+    google_storage_bucket.output_bucket,
+    time_sleep.wait_iam_binding
   ]
 }
 
