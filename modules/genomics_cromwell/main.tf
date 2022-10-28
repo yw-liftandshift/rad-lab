2b53e6f64960d8e983db7e4f052d8a8819c52fa7
Maksym Veliaminov <maksym.veliaminov@yoppworks.com>
2022-10-28 12:03:50 -0400

    Add budget notifications topic

--- modules/genomics_cromwell/main.tf
+++ modules/genomics_cromwell/main.tf
@@ -218,7 +218,8 @@ resource "google_billing_budget" "budget" {
   all_updates_rule {
     monitoring_notification_channels = [
       google_monitoring_notification_channel.scientist_notification_channel.id,
-      google_monitoring_notification_channel.manager_notification_channel.id
+      google_monitoring_notification_channel.manager_notification_channel.id,
+      var.budget_notifications_topic
     ]
     disable_default_iam_recipients = true
   }
