f5928bf755e4f0f6fd1373c3fd3b2666e2a5bea1
Maksym Veliaminov <maksym.veliaminov@yoppworks.com>
2022-11-02 12:28:17 -0400

    Add budget notification topic var

--- modules/data_science/variables.tf
+++ modules/data_science/variables.tf
@@ -237,3 +237,7 @@ variable "manager" {
   description = "Manager who approved the project"
   type        = string
 }
+
+variable "budget_notifications_topic" {
+  description = "Pub/Sub topic for budget status updates"
+}
